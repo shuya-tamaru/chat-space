a0e6331b77a069d9112695230e26d14f25251351
shuya-tamaru <tamaru1013@gmail.com>
2020-01-03 15:22:29 +0900

    rewrite_unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '52.68.106.105', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -59,4 +61,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.68.106.105', user: 'ec2-user', roles: %w{app db web}
+
